7506dc110d2c74a050e026f626b16f7516b0181d
brux <bgonsor@protonmail.com>
2021-08-27 11:48:57 -0500

    moving files to source

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-const express = require("express");
-const app = express();
-const path = require("path");
+const express = require("express"); // Requiring the express module
+const app = express(); // Setting the express module to the constant app
+const path = require("path"); // Necessary to find the paths of files
+const router = require("./routes/mainRoutes"); // Connecting the router to communicate with the views
 
+// Establishing the path for requiring static resources
 const publicPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicPath));
+
+// Setting the view engine
+app.set("view engine", "ejs");
+
+// Setting all routes to be obtained from the routes file
+app.use("/", router);
 
 // Waking the server
 let port = process.env.PORT || 3000;
